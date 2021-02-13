5bcff0e59b36957d53370449dbf85f91a0d63bb3
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2021-02-13 15:06:29 -0500

    Test

--- GameJam/Assets/Scripts/Networking/NetworkManagerUtils/PlayerInstantiate.cs
+++ GameJam/Assets/Scripts/Networking/NetworkManagerUtils/PlayerInstantiate.cs
@@ -10,19 +10,19 @@ public class PlayerInstantiate : MonoBehaviour
 
     private void Awake()
     {
-        if (!PhotonNetwork.IsConnected) //If Player is connected to Photon Network then Instantiate Server Side
+        if (!PhotonNetwork.IsConnected)
         {
             Vector2 offset = Random.insideUnitCircle * 3.0f;
             Vector3 position = new Vector3(transform.position.x + offset.x, transform.position.y, transform.position.z);
             NetworkingManager.InstantiateOverNetwork(player_, position, Quaternion.identity);
-            GameObject.FindObjectOfType<Camera>().enabled = true;
+            GameObject.FindObjectOfType<Camera>().enabled = true; 
         }
 
-        else //Otherwise just instantiate locally
+        else if(PhotonNetwork.IsConnected)
         {
             Debug.Log("Creating local player");
             Instantiate(player_, transform.position, Quaternion.identity);
-            GameObject.FindObjectOfType<Camera>().enabled = true;          
+            GameObject.FindObjectOfType<Camera>().enabled = true;
         }
     }
 }
