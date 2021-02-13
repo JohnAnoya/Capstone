d1b4e1694510f32de8dbfd7ba6aa84bd33e5fb7b
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2021-02-13 15:12:24 -0500

    Player Instantiation should be fixed for Server and Client Side

--- GameJam/Assets/Scripts/Networking/NetworkManagerUtils/PlayerInstantiate.cs
+++ GameJam/Assets/Scripts/Networking/NetworkManagerUtils/PlayerInstantiate.cs
@@ -10,15 +10,16 @@ public class PlayerInstantiate : MonoBehaviour
 
     private void Awake()
     {
-        if (!PhotonNetwork.IsConnected)
+        if (PhotonNetwork.IsConnected && PhotonNetwork.InRoom)
         {
+            Debug.Log("CREATING SERVER SIDE PLAYER");
             Vector2 offset = Random.insideUnitCircle * 3.0f;
             Vector3 position = new Vector3(transform.position.x + offset.x, transform.position.y, transform.position.z);
             NetworkingManager.InstantiateOverNetwork(player_, position, Quaternion.identity);
-            GameObject.FindObjectOfType<Camera>().enabled = true; 
+            GameObject.FindObjectOfType<Camera>().enabled = true;
         }
 
-        else if(PhotonNetwork.IsConnected)
+        else 
         {
             Debug.Log("Creating local player");
             Instantiate(player_, transform.position, Quaternion.identity);
