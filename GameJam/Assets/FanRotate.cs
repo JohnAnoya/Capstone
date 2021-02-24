922b9200d92150da6fa413ffbfda7bb1ff6b069c
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2021-02-24 17:44:04 -0500

    Added Fan to Room 4

---
+++ GameJam/Assets/FanRotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanRotate : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(1000.0f * Time.deltaTime, 0.0f, 0.0f); 
+    }
+}
