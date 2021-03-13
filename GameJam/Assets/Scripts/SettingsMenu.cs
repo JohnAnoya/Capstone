e026cb537a6f028f62706ccafb0565a3d2fa116b
AlexanderDispenza <alexanderdispenza@gmail.com>
2021-03-13 16:16:16 -0500

    Setting Menu Functionality
    Added:
    -Music & Sound effect volume slider functionality.
    - Fps display toggle functionality.

---
+++ GameJam/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+	public AudioMixer musicMixer;
+	public AudioMixer sfxMixer;
+	
+	public Canvas fpsCanvas;
+	public bool fpsCountActive = false;
+
+	private void Start()
+	{
+		FpsDisplayCanvas();
+	}
+
+	public void SetMusicVolume(float volume_) 
+	{
+		//Converting slider to use logarithmic scale instead of a linear one 
+		musicMixer.SetFloat("MusicVolume", Mathf.Log10(volume_) * 20);
+	}
+
+	public void SetSfxVolume(float volume_)
+	{
+		//Converting slider to use logarithmic scale instead of a linear one 
+		sfxMixer.SetFloat("SfxVolume", Mathf.Log10(volume_) * 20);
+	}
+
+	public void FpsDisplayCanvas() 
+	{
+		if (!fpsCountActive)
+		{
+			fpsCanvas.enabled = true;
+			fpsCountActive = true;
+		}
+		else if (fpsCountActive)
+		{
+			fpsCanvas.enabled = false;
+			fpsCountActive = false;
+		}
+	}
+
+}
